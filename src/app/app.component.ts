c9c9ff0af7249fe8a818a23fe741c5f3c6b2cc6b
HuyLC <target.luonghuy@gmail.com>
2017-10-24 23:05:47 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface DataResponse{
+  data: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  imgs: any;
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<DataResponse>('https://graph.facebook.com/v2.10/1173636692750000/feed?fields=id,full_picture,created_time,message,likes,from,place,object_id&limit=50&access_token=EAAAAUaZA8jlABANZBfQhn4rw9ZBZCRO1TRv2NcEyecrq88tOKgj9mkxkColJmVx8DsN4a0y1yBy9zoEhPqTtHCcuOVDj99uBMy6RtuROX9DYTkZCGjpiTrkfaAxpMkcZC8OP97QaLuEKVX7MzixKf6LTlsiQmLlWkZD')
+    .subscribe(
+      data => {
+         this.imgs = data.data;
+      }
+  );
+  }
 }
